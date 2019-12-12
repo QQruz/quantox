cc134f25b704b6ced01fb0e035cc7b865ae4758f
QQruz <bbojan@protonmail.com>
2019-12-12 14:42:20 +0100

    Add config.example

---
+++ config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_NAME', '');
+
+define('DB_HOST', '');
+
+define('DB_PORT', 3306);
+
+define('DB_USER', '');
+
+define('DB_PASS', '');
+
+define('VIEWS_PATH', __DIR__ . '/src/Views/');
